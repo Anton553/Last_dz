c15222ab180f98df00dc6e1cb4195f752150589b
Anton <Anton@pochta.ru>
2024-05-12 10:48:45 +0300

    Добавили заполнение массива с условием

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿string[] array1 = new string[6] {"test1", "342", "dragon", "world", "ini", "test2"};
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+string[] array1 = new string[6] {"test1", "342", "dragon", "world", "ini", "test2"};
 string[] array2 = new string[array1.Length];
 
 void PrintArray(string[] array)
@@ -10,3 +15,19 @@ void PrintArray(string[] array)
     Console.WriteLine();
 }
 
+void Array2IF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+Array2IF(array1, array2);
+Console.WriteLine("Выводим строки длина которых меньше, либо равна 3 символам :");
+PrintArray(array2);
