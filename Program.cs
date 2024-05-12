c0d97d90b82c7569decb7ad1adf4d7a70256e983
Anton <Anton@pochta.ru>
2024-05-12 10:43:27 +0300

    Добавили вывод массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿string[] array1 = new string[6] {"test1", "342", "dragon", "world", "ini", "test2"};
 string[] array2 = new string[array1.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
